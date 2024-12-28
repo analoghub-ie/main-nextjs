d6372c1c596f853d16e9fb4a02d4413123e096d3
Timofey Klimov <klimofey777@gmail.com>
2024-12-28 11:58:30 +0200

    add redirect from unknown paths

---
+++ app/category/page.tsx
@@ -0,0 +1,13 @@
+import {redirect, RedirectType} from "next/navigation";
+
+
+export default async function Page() {
+
+    redirect('/', RedirectType.replace);
+
+    // return (
+    //     <div className="flex flex-col gap-4 overflow-auto">
+    //
+    //     </div>
+    // );
+}
