7d93a8102bcd5f1b1f88ac0e4b3d445152919eca
Timofey Klimov <klimofey777@gmail.com>
2025-01-04 23:26:04 +0200

    add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,41 @@
+import {MarkdownRenderer} from "@/components/markdownWrapper";
+import {AnalogAvatar} from "@/svg/avatar";
+import {Breadcrumbs} from "@/components/breadcrumbs";
+
+export default function About() {
+    return (
+        <>
+            <Breadcrumbs elements={[
+                {title: 'Home', href: '/'},
+                {title: 'About', href: `/about`}
+            ]}/>
+
+            <div className="flex flex-col-reverse lg:flex-row items-center lg:items-start lg:gap-8">
+
+
+                <div className="lg:w-2/3 text-left">
+                    <MarkdownRenderer>
+                        {`
+Hi everyone, my name is [Aleksandr Sidun](https://www.linkedin.com/in/sidun-av/), I am an Analog IC Designer and enthusiast.
+
+Analog IC design is a very interesting and challenging job, but the lack of information, user-unfriendly software and knowledge gaps are opposing engineers.
+
+My goal is to create a one-stop website, where all Analog IC design engineers can get and share useful information. I will post useful scripts, tutorials, documentation and equations into one knowledge base that will hopefully get bookmarked :)
+
+I also believe that analog design is missing some sort of "code style" that should make our designs more clear and friendly.
+
+All the information provided on this website is free. You can use everything for personal purposes for free. If you find any mistakes or want to contribute with your own article/script/documentation, please contact me by email:
+[suggestions@analoghub.ie](mailto:suggestions@analoghub.ie)
+
+Special thanks to [Timofey Klimov](https://www.linkedin.com/in/klimofey/), who made my ideas come true by creating this website.
+          `}
+                    </MarkdownRenderer>
+                </div>
+
+                <div className="lg:w-1/3 flex justify-center lg:justify-start mb-4">
+                    <AnalogAvatar />
+                </div>
+            </div>
+        </>
+    );
+}
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,32 +1,43 @@
 'use client';
 
 import {Divider, Link, Spacer} from "@nextui-org/react";
-import {AiFillLinkedin, AiOutlineMail} from "react-icons/ai";
-
+import {AiFillLinkedin, AiOutlineInfoCircle, AiOutlineMail} from "react-icons/ai";
 
 export const Footer = () => {
-
     return (
-        <footer className="py-6 mt-auto bg-background text-text">
-            <Divider/>
+        <footer className="py-8 mt-auto bg-background text-text">
+            <Divider />
 
-            <Spacer y={6}/>
+            <Spacer y={8} />
 
             <div className="container mx-auto text-center">
-                <div className="flex justify-center gap-6">
-                    <Link href="mailto:contact@analoghub.ie" target="_blank" className="flex items-center gap-2">
-                        <AiOutlineMail size={24} className="text-text"/>
+                <div className="flex flex-col sm:flex-row justify-center gap-6">
+                    <Link href="mailto:contact@analoghub.ie"
+                          target="_blank"
+                          className="flex items-center gap-2"
+                    >
+                        <AiOutlineMail size={24} className="text-text" />
                         <span className="hidden sm:inline">contact@analoghub.ie</span>
                     </Link>
 
-                    <Link href="https://www.linkedin.com/in/sidun-av/" target="_blank"
-                          className="flex items-center gap-2">
-                        <AiFillLinkedin size={24} className="text-text"/>
+                    <Link href="https://www.linkedin.com/in/sidun-av/"
+                          target="_blank"
+                          className="flex items-center gap-2"
+                    >
+                        <AiFillLinkedin size={24} className="text-text" />
                         <span className="hidden sm:inline">Aleksandr Sidun</span>
+                    </Link>
+
+                    {/* Added link to /about */}
+                    <Link href="/about"
+                          className="flex items-center gap-2"
+                    >
+                        <AiOutlineInfoCircle size={24} className="text-text" />
+                        <span>About Us</span>
                     </Link>
                 </div>
 
-                <Spacer y={3}/>
+                <Spacer y={4} />
 
                 <p className="text-small">
                     &copy; 2024-{new Date().getFullYear()} All Rights Reserved
