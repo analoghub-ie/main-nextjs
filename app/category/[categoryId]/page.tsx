c94f040e021c201ef02d8f3b0579a1c02fa95e22
Timofey Klimov <klimofey777@gmail.com>
2025-04-01 19:59:09 +0300

    add "XX more articles on home page"

--- app/category/[categoryId]/page.tsx
+++ app/category/[categoryId]/page.tsx
@@ -1,13 +1,53 @@
+import {allFilteredArticles} from "@/articles/allArticles";
+import {Link} from "@nextui-org/react";
+import {Metadata} from "next";
 import {redirect, RedirectType} from "next/navigation";
+import {Breadcrumbs} from "@/components/breadcrumbs";
 
+type Props = {
+    params: Promise<{ categoryId: string }>;
+};
 
-export default async function Page() {
+export async function generateMetadata({ params }: Props) {
+    const { categoryId } = await params;
 
-    redirect(`/`, RedirectType.replace);
+    const category = allFilteredArticles.find((category) => category.id === categoryId);
 
-    // return (
-    //     <div className="flex flex-col gap-4 overflow-auto">
-    //
-    //     </div>
-    // );
+    return {
+        title: category?.title,
+    } satisfies Metadata
+}
+
+export default async function Page({params}: Props) {
+
+    const { categoryId } = await params;
+
+    // Find the category and article
+    const category = allFilteredArticles.find((category) => category.id === categoryId);
+
+    if (!category) {
+        // Category not found -> redirect to home
+        redirect('/', RedirectType.replace);
+    }
+
+    return (
+        <div className="flex flex-col gap-4 overflow-auto">
+            <Breadcrumbs elements={[
+                {title: 'Home', href: '/'},
+                {title: category.title, href: `/category/${categoryId}`},
+            ]}/>
+
+            <div className="flex flex-col gap-1 overflow-auto">
+                {category.articles.map((article) => (
+                    <Link
+                        key={article.id}
+                        href={`/category/${category.id}/article/${article.id}`}
+                        style={{ display: "block", marginBottom: "0.5rem" }}
+                    >
+                        {article.title}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 }
